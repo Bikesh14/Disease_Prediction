1f3e3b92eb92dfb0c550f048821c8e2a46cceab9
saugat poudel <saugat.poudel478@gmail.com>
2022-12-11 22:44:13 +0545

    'summarizer

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+from urllib import request
 from flask import Flask, render_template, url_for
 
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
+
+
+@app.route("/", methods=("GET", "POST"))
+def transcribe():
+    string = ''
+
+    if request.method == 'POST':
+        return render_template('index.html')
+
     return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
