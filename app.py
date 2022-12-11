1a340b7547d5f7d4ee919969cee6c2ca11b8d3af
Bikesh Bimali <bimalibikesh@gmail.com>
2022-12-12 00:04:38 +0545

    disease prediction model added

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd 
+import numpy as np
+import predengine
 
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    symptoms='itching,skin_rash,chills,shivering'
+    prediction=predengine.predict_disease(symptoms)
+
+    return render_template('index.html', prediction=prediction)
+    # return render_template('index.html')
+    
+
+
+# @app.route('/', methods=['POST'])
+# def disease_predict():
+    # symptoms=[x for x in request.form.values()]
+    # features=['itching,skin_rash,chills,shivering']
+   
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
+
